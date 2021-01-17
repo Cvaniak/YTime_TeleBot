41258a5938fa391eec3f12e579d17125a1663841
Cvaniak <igna.cwaniak@gmail.com>
2021-01-17 08:35:40 +0100

    First Version of YTime_duration bot

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # YTime_TeleBot
+Template and first steps based on [codementor tutorial](https://www.codementor.io/@karandeepbatra/part-1-how-to-create-a-telegram-bot-in-python-in-under-10-minutes-19yfdv4wrq).
+Google staff [here](https://console.developers.google.com/) and here [here](https://developers.google.com/youtube/v3/quickstart/python).
+---
+First install ```pip install -r requirements.txt```  
+Then add file ```.env``` with:
+```
+TOKEN="BOT TOKEN FROM TELEGRAM WEBSITE"
+YT_KEY="KEY FROM GOOGLE CONSOLE"
+```
+Then run with ```python3 main.py
---
+++ mian.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+import os
+import isodate
+from dotenv import load_dotenv
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from googleapiclient.discovery import build
+load_dotenv()
+
+service = build("youtube", "v3", developerKey=os.getenv("YT_KEY"))
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+def start(update, context):
+    """Send a message when the command /start is issued."""
+    update.message.reply_text('Hi!')
+
+def help(update, context):
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Help!')
+
+def echo(update, context):
+    """Echo the user message."""
+    mess_t = update.message.text
+    try:
+        if mess_t.find("youtube.com/watch?v="):
+            start_id = mess_t.find("v=")+2
+            end_id = mess_t.find("&", start_id)
+            id_v = mess_t[start_id:end_id]
+            print(id_v)
+            request = service.videos().list(part="snippet,contentDetails", id=id_v)
+            response = request.execute()
+            duration = response["items"][0]["contentDetails"]["duration"]
+            duration = isodate.parse_duration(str(duration))
+            title = response["items"][0]["snippet"]["title"]
+            update.message.reply_text(F"{title} ({duration})")
+
+    except Exception as e:
+        print(e)
+        pass
+
+
+def error(update, context):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+
+def main():
+    """Start the bot."""
+    updater = Updater(os.getenv("TOKEN"), use_context=True)
+
+    dp = updater.dispatcher
+
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+
+    dp.add_handler(MessageHandler(Filters.text, echo))
+
+    dp.add_error_handler(error)
+
+    updater.start_polling()
+
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
